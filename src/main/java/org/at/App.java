adecf20a8cb58d2eefb27af5f86af445fd7f11d6
ArchmageTony <archmagetony@gmail.com>
2020-08-10 14:18:28 +0800

    style: 增加注释
    Signed-off-by: ArchmageTony <archmagetony@gmail.com>

--- src/main/java/org/at/App.java
+++ src/main/java/org/at/App.java
@@ -8,8 +8,8 @@ import java.io.File;
  * @date 2020/8/3 13:35
  */
 public class App {
-    public static final String USER_DIR = System.getProperty("user.dir");
-    public static final String KCCM_SETTING = USER_DIR + File.separator + "KCCM Setting.ini";
+    public static final String USER_DIR = System.getProperty("user.dir");//获取用户程序所在地址
+    public static final String KCCM_SETTING = USER_DIR + File.separator + "KCCM Setting.ini";//设置配置文件所在地址与名称
 
     public static void main(String[] args) {
         new AppInit().init();
